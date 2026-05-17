8f3a836e6de989c22350aba446fde726ba54b026
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-17 11:12:50 +0530

    Solid Pyramid Printing

--- Patterns.java
+++ Patterns.java
@@ -31,3 +31,22 @@ class InvertedPattern {
         }
     }
 }
+
+// Solid Pyramid Pattern
+class SolidPyramid{
+    public static void main(String[] args){
+        int n = 5;
+        // Rows Outer Loop( 1 --> n)
+        for( int i = 1; i<=n ; i++){
+            //Space Printing ( 1 --> n-1)
+            for(int j = 1; j <= n-i ; j++){
+                System.out.print("  ");
+            }
+            //Star Printing ( 1 --> 2*i-1)
+            for(int k = 1; k<=2*i-1 ; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
