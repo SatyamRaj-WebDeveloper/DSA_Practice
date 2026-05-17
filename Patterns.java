d35ac68c96041c33f850e175ceb1cd871e67e0eb
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-17 10:55:44 +0530

    Inverted Right Angled triangle

--- Patterns.java
+++ Patterns.java
@@ -15,3 +15,19 @@
         }
     }
 }
+
+// Inverted Half Pyramid Pattern
+class InvertedPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        //First for Loop for Rows
+        for( int i = 1 ; i<=n ;i++){
+            //Second for Loop for Colums (1 --> n-i+1)
+            for(int j = 1; j<= n-i+1 ;j++){
+                System.out.print("* ");
+            }
+            //New Line
+            System.out.println();
+        }
+    }
+}
