cdf359085cbf5e7efdae8be8e2bce5da69b55969
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-17 10:40:59 +0530

    Pattern : Rhombus

---
+++ Patterns.java
@@ -0,0 +1,17 @@
+// Rhombus Pattern
+ class RhombusPattern {
+    public static void main(String[] args){
+        int n = 5;
+        for(int i = 1 ; i<= n;i++){
+            // Space Printing (1 --> n-row number)
+            for(int j = 1 ;j <=n-i;j++){
+                System.out.print(" ");
+            }
+            // Star Printing (1 --> n number of stars )
+            for(int k=1;k<=n;k++){
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
+    }
+}
