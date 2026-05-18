6e3d9d2e69cbc59abfcd74c9ab4960006b54344b
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-18 10:37:26 +0530

    Invereted Pyramid

--- Patterns.java
+++ Patterns.java
@@ -38,8 +38,8 @@ class SolidPyramid{
         int n = 5;
         // Rows Outer Loop( 1 --> n)
         for( int i = 1; i<=n ; i++){
-            //Space Printing ( 1 --> n-1)
-            for(int j = 1; j <= n-i ; j++){
+            //Space Printing ( 1 --> n-i)
+            for(int j = 1; j <= n-i ; j++){ 
                 System.out.print("  ");
             }
             //Star Printing ( 1 --> 2*i-1)
@@ -47,6 +47,23 @@ class SolidPyramid{
                 System.out.print("* ");
             }
             System.out.println();
+        }
+    }
+}
+
+class InvertedSolidPyramid{
+    int n = 4 ;
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 1 ; i<=n ; i++){
+            
+           for( int j =1 ;j <= i-1 ; j++){
+            System.out.print("  ");
+           }
+           for(int k = 1 ;k<= 2*n-2*i+1 ;k++){
+            System.out.print("* ");
+           }
+           System.out.println();
         }
     }
 }
