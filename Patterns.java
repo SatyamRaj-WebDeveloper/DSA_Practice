cb6409c7ac762e159c06125544c7be9cee309822
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-19 11:06:18 +0530

    Patter : Third Numeric Patter

--- Patterns.java
+++ Patterns.java
@@ -265,3 +265,14 @@ class NumericSecond{
     }
 }
 
+class NumericThird{
+    public static void main(String[] args) {
+        int n = 5;
+        for(int row = 1 ; row <= n ; row++){
+            for(int col = 1 ; col<= row ; col++){
+                System.out.print((char)(64+col) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
