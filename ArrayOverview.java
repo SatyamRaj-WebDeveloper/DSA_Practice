9640efe9fec66f62991a9a7f9aaebfd60d2cbf5d
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-21 11:20:49 +0530

    2D Array : Accessing elements with variable size

--- ArrayOverview.java
+++ ArrayOverview.java
@@ -56,3 +56,19 @@ class FindMax{
         System.out.print(max);
     }
 }
+
+// 2D (2 Dimensional Arrays) --> Array of Arrays
+class TwoDimensionalArray{
+    public static void main(String[] args) {
+        int arr[][] = { {1,2} , {2,3,4,5,6} , {4,5,6,78,9,15,20} , {5}};
+        int rowLength = arr.length;
+        for(int rowIndex = 0 ; rowIndex <= rowLength-1 ; rowIndex++){
+            int colLength = arr[rowIndex].length;
+            for(int j =0 ; j<=colLength-1 ; j++){
+                System.out.print(arr[rowIndex][j]+" ");
+            }
+            System.err.println();
+        }
+
+    }
+}
