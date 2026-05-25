1ed077c79746e1604e1b69e34add1ab92be463fc
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-25 09:49:54 +0530

    Basic Maths: Calculating LCM

--- BasicMath.java
+++ BasicMath.java
@@ -1,5 +1,12 @@
 //Calculating GCD 
 class Calculating{
+
+    static int getLCM(int a , int b){
+        int gcd = getGCD(a, b);
+        int prod = a*b;
+        return prod/gcd;
+    }
+
     static int getGCD(int a , int b){
         //gcd(a,b) = gcd(b , a%b)
         while( b != 0){
@@ -12,5 +19,6 @@ class Calculating{
     }
     public static void main(String[] args){
         System.out.println(getGCD(12, 18));
+        System.out.println(getLCM(12,18));
     }
 }
