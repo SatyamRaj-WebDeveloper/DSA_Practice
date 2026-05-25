934b82a75d64958dfec97548cf80cabd09db44ef
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-25 10:03:55 +0530

    Basic Math : Armstrong Number

--- BasicMath.java
+++ BasicMath.java
@@ -22,3 +22,24 @@ class Calculating{
         System.out.println(getLCM(12,18));
     }
 }
+
+class Armstrong{
+    public static void main(String[] args) {
+        int num = 153 , sum =0;
+        int originalNum = num;
+        while( num != 0){
+            //extracting Last digit
+            int rem = num % 10;
+            //Calculating cube and adding it to Sum
+            int cube = rem*rem*rem;
+            sum += cube;
+            //Removing last digit from original Number
+            num =num /  10;
+        }
+        if(sum == originalNum){
+            System.out.println("Armstrong Number");
+        }else{
+            System.out.println("Not Armstrong");
+        }
+    }
+}
