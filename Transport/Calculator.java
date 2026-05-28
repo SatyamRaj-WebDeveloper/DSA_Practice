c22802538fefa5fbc81e54c866bc881d958c0c07
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-28 10:49:06 +0530

    DSA Practice: Code implementation for Polymorphism (Runtime)

--- Transport/Calculator.java
+++ Transport/Calculator.java
@@ -1,4 +1,6 @@
+
 public class Calculator{
+    // COMPILE TIME POLYMORPHISM
     int add(int a , int b){
         return a+b;
     }
@@ -6,6 +8,7 @@ public class Calculator{
     int add(int a, int b , int c){
         return a+b+c;
     }
+    //Method Overloading
     double add(double a  , double b , int c){
         return a+b+c;
     }
@@ -17,5 +20,34 @@ class Test{
         System.out.println(c.add(1,5));
         System.out.println(c.add(2.4,5.8,6));
         System.out.println(c.add(5,8,3));
+
+
+    }
+}
+
+class shape{
+    public void draw(){
+        System.out.print("Drawing Shape");
+    }
+}
+class Rectangle extends shape{
+    //METHOD OVERRIDING --> Runtime Polymorphism
+    public void draw(){
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+class Circle extends shape{
+    //METHOD OVERRIDING --> Runtime Polymorphism 
+    public void draw(){
+        System.out.println("Drawing Circle");
+    }
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        c.draw();
+        Rectangle rec = new Rectangle();
+        rec.draw();
+        shape sh =  new shape();
+        sh.draw();
     }
 }
