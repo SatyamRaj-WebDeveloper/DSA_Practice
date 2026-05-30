971cd7e75189a29fb88a2e54f2ab267ade3679ba
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-05-30 11:43:29 +0530

    Queue:code implementation using LinkedList

--- CollectionsCode.java
+++ CollectionsCode.java
@@ -1,54 +1,77 @@
 import java.util.*;
 
-class Collections{
+// class Collections{
+//     public static void main(String[] args) {
+//         //3 ways to create an ArrayList<>();
+//          ArrayList<Integer> arl = new ArrayList<>();
+//         //  List<Integer> arr = new ArrayList<>();
+//         //  Collection<Integer> ar1 = new ArrayList<>();
+
+//         arl.add(10);
+//         arl.add(20);
+//         arl.add(40);
+//         arl.add(50);
+//         System.err.println(arl);
+//         arl.remove(0);
+//         System.out.println(arl);
+
+//         //addAll --> Adds(pushes) all the elements of a list to another list
+//         List<Integer> list =  new ArrayList<>();
+//         list.addAll(arl);
+//         System.out.println(list);
+
+//         list.add(201);
+//         list.add(501);
+//         list.removeAll(arl); // removeAll() --> removes all the elements of any List from a List
+//         System.err.println(list);
+
+//         System.out.println(list.size());
+//         // arl.clear();
+//         // System.out.println(arl);
+        
+//         //List Traversal using Iterator
+//         // Iterator<Integer> itr = arl.iterator();
+//         // while(itr.hasNext()){
+//         //     System.out.println("ELement :" + itr.next());
+//         // }
+        
+//         LinkedList<Integer> linkList = new LinkedList<>();
+//         linkList.add(40);
+//         linkList.add(30);
+//         linkList.add(10);
+//         linkList.add(80);
+
+//         //addFirst() and addLast()
+//         linkList.addFirst(100);
+//         linkList.addLast(90);
+//         System.out.println(linkList);
+        
+//         System.out.println(linkList.poll());
+//         linkList.offer(3); // offer() adds element to the last of the list 
+//         System.out.println(linkList);
+
+//     }
+// }
+
+class QueueBasics{
     public static void main(String[] args) {
-        //3 ways to create an ArrayList<>();
-         ArrayList<Integer> arl = new ArrayList<>();
-        //  List<Integer> arr = new ArrayList<>();
-        //  Collection<Integer> ar1 = new ArrayList<>();
-
-        arl.add(10);
-        arl.add(20);
-        arl.add(40);
-        arl.add(50);
-        System.err.println(arl);
-        arl.remove(0);
-        System.out.println(arl);
-
-        //addAll --> Adds(pushes) all the elements of a list to another list
-        List<Integer> list =  new ArrayList<>();
-        list.addAll(arl);
-        System.out.println(list);
-
-        list.add(201);
-        list.add(501);
-        list.removeAll(arl); // removeAll() --> removes all the elements of any List from a List
-        System.err.println(list);
-
-        System.out.println(list.size());
-        // arl.clear();
-        // System.out.println(arl);
+        /*Prefered Methods --> 
+         offer() to add ,
+         poll() to return and remove the head of queue , 
+         peek() to return the head of the queue*/
+        Queue<Integer> q =  new LinkedList<>();
+        // Deque<Integer> dq = new ArrayDeque<Integer>();
         
-        //List Traversal using Iterator
-        // Iterator<Integer> itr = arl.iterator();
-        // while(itr.hasNext()){
-        //     System.out.println("ELement :" + itr.next());
-        // }
-        
-        LinkedList<Integer> linkList = new LinkedList<>();
-        linkList.add(40);
-        linkList.add(30);
-        linkList.add(10);
-        linkList.add(80);
-
-        //addFirst() and addLast()
-        linkList.addFirst(100);
-        linkList.addLast(90);
-        System.out.println(linkList);
-        
-        System.out.println(linkList.poll());
-        linkList.offer(3); // offer() adds element to the last of the list 
-        System.out.println(linkList);
-
+        q.add(10);
+        q.add(20);// add() --> returns exception if fails to add.
+        q.offer(30);//offer --> is prefered because add() return exception if fails , but offer() does not
+        q.offer(50);
+        q.offer(90);
+        q.offer(100);
+        System.out.println(q);
+        q.remove();
+        System.out.println(q);
+        System.out.println(q.poll());
+        System.out.println(q);
     }
 }
