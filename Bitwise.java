ef32223b9dd0a488bc3ef002822b7fef6a58857c
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2026-06-03 09:41:57 +0530

    Swap two number using XOR Operator

--- Bitwise.java
+++ Bitwise.java
@@ -49,3 +49,15 @@ class CheckPowerOfTwo{
         }
     }
 }
+
+class SwapTwoNumbers{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.err.println("a = " + a);
+        System.err.println("b = " + b);
+    }
+}
